92e6aa4b061adebc681bcdc30040c8718883130d
CarlosBarbosaFilho <cbarbosagomesfilho@gmail.com>
2018-08-28 21:12:35 -0300

    Resolvam o problema do cadastro de contato

--- src/br/com/agenda/model/Contato.java
+++ src/br/com/agenda/model/Contato.java
@@ -3,10 +3,34 @@
  */
 package br.com.agenda.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * @author carlosbarbosagomesfilho
  *
  */
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="contatos")
 public class Contato {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nome;
 }
--- src/br/com/agenda/servlets/ContatoServlet.java
+++ src/br/com/agenda/servlets/ContatoServlet.java
@@ -1,11 +1,15 @@
 package br.com.agenda.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.com.agenda.model.Contato;
+import br.com.agenda.regras.ContatoRN;
 
 /**
  * Servlet implementation class ContatoServlet
@@ -14,17 +18,26 @@ import javax.servlet.http.HttpServletResponse;
 public class ContatoServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private ContatoRN service;
        
     public ContatoServlet() {
-        super();
+    	
+    	this.service = new ContatoRN();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		
+		Contato contato = new Contato();
+		
+		
+		String nome = request.getParameter("nome");
+		contato.setNome(nome);
+		
+		this.service.addContato(contato);
+		
+		System.out.println(">>>>>>>>>>>>>>>>>>>> CONTATO " + contato.getNome()+ "SALVO COM SUCESSO !");
+		
 	}
 
 }
